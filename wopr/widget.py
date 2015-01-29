a1a8e6444835a3cf54ca3932a3b62c31e306e342
Chris Carter <chris@gibsonsec.org>
2015-01-30 10:29:49 +1100

    Hm.

--- wopr/widget.py
+++ wopr/widget.py
@@ -37,7 +37,7 @@ class Widget(object):
 
 	def draw_canvas(self, canvas, attr=0):
 		for y, line in enumerate(canvas.frame().split("\n")):
-			self.scr.addstr(y, 0, line, attr)
+			self.scr.insstr(y, 0, line, attr)
 
 	def draw(self):
 		pass
@@ -58,23 +58,28 @@ class Sparkline(Widget):
 
 		buffer = 20
 		data_buffer = 3
-		# We can only fix (x*2) points of data. Also account for buffers etc.
+		# We can only show (x*2) points of data. Also account for padding etc.
 		data = list(self.data)[-((maxX-2-(buffer+data_buffer))*2):]
+		max_point = max(data)
+		max_axes = (max_point // 10) * 10
 
 		self.canvas.clear()
 		self.axes.clear()
 
+		## AXES ###############################################################
 		self.axes.set(0, 0)
+		self.scr.addstr(1, 1, str(max_axes))
 		for y in range(buffer, (maxY)*4 - buffer):  # left axes
 			self.axes.set(buffer, y)
 		for x in range(buffer, (maxX)*2 - buffer):  # bottom axes
 			# (maxY*4)-buffer is (maxY*8)/2 for the edge.
 			self.axes.set(x, (maxY)*4 - buffer)
 
+		## DATA ###############################################################
 		self.canvas.set(0, 0)
 		for i, point in enumerate(data):
 			# (maxY*2) is the average for the widget's center point.
-			self.canvas.set(i+buffer+data_buffer, ((maxY*4) / 2) + point)
+			self.canvas.set(i-buffer+data_buffer, (maxY*4) - buffer - data_buffer - point)
 
 		self.draw_canvas(self.canvas, attr=curses.color_pair(2))
 		self.draw_canvas(self.axes, attr=curses.color_pair(0))
@@ -84,6 +89,6 @@ class Sparkline(Widget):
 
 class RandomSparkline(Sparkline):
 	def draw(self):
-		p = self.data[-1] + random.choice([1, 0, 0, -1])
+		p = self.data[-1] + random.choice([1, 1, 0, 0, -1])
 		self.add_point(p if p > 0 else 0)
 		super(RandomSparkline, self).draw()
